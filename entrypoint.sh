3394a9d3d3d8febe981830dabd01db6a9a4fdb82
fjgarcia164 <fjgarcia164@ieszaidinvergeles.org>
2025-11-19 14:50:32 +0100

    sol

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-# 1. Detiene el script si una migración o seeder falla
+# Este script ya no ejecuta la migración/seeding (Se hará a mano)
 set -e
-# entrypoint.sh (Añadir debajo de set -e)
 
-# Esperar 30 segundos para que la BBDD termine de arrancar y esté accesible
-sleep 120
-echo "Iniciando secuencia de inicialización..."
+# 1. Limpia Caché (Esencial para evitar errores 404 de rutas)
+php artisan route:clear
+php artisan config:clear
 
-# 2. Comandos de Mantenimiento y Seeding
-php artisan route:clear      # Limpia rutas
-php artisan config:clear     # Limpia configuración
-php artisan db:seed          # Crea el Usuario Dummy (Fix 23503)
-php artisan migrate --force  # Asegura la existencia de tablas
-
-# 3. Dar permisos finales
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-
-echo "Inicialización terminada. Arrancando servidor Apache..."
-
-# 4. Comando de arranque principal (entrega el control al contenedor)
-# entrypoint.sh - ÚLTIMA LÍNEA
+# 2. Comando de arranque principal (entrega el control al contenedor)
 exec "$@"
