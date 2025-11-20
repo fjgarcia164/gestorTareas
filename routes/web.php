5e3513951882a3e6a43efc7322504f4eecbcead5
fjgarcia164 <fjgarcia164@ieszaidinvergeles.org>
2025-11-20 16:24:02 +0100

    subtareas y comentarios

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,14 @@ Route::middleware('auth')->group(function () {
     Route::resource('categorias', CategoriaController::class);
     Route::resource('tareas', TareaController::class);
 
+    Route::resource('tareas', TareaController::class);
+
+    Route::post('/tareas/{id}/subtareas', [App\Http\Controllers\SubtareaController::class, 'store'])->name('subtareas.store');
+    Route::patch('/subtareas/{id}', [App\Http\Controllers\SubtareaController::class, 'update'])->name('subtareas.update');
+    Route::delete('/subtareas/{id}', [App\Http\Controllers\SubtareaController::class, 'destroy'])->name('subtareas.destroy');
+
+    Route::post('/tareas/{id}/comentarios', [App\Http\Controllers\ComentarioController::class, 'store'])->name('comentarios.store');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
